6bab3b4c01c97ce512f47df2a147e1a8e877f53c
Lukas Nielsen <moin@lukasnielsen.de>
2025-04-20 22:13:18 +0200

    add cors

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
-
+// Allow from any origin
 header("Access-Control-Allow-Origin: *");
+
+// Optional: Handle preflight requests (for complex requests)
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type, Authorization");
+	exit(0);
+}
+
 header("Content-Type: text/plain");
 
 echo $_SERVER["REMOTE_ADDR"];
